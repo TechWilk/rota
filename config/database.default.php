f11d789f23a01cef9ae8324846a18a905c7fb15f
Christopher Wilkinson <git@wilk.tech>
2017-12-30 10:55:23 +0000

    Remove unused config options

--- config/database.default.php
+++ config/database.default.php
@@ -14,10 +14,6 @@ $config['db']['pass'] = 'password';
 // Unless your host tells you differently, this should remain as 'localhost'
 $config['db']['host'] = 'localhost';
 
-// Don't change unless multiple installs are required in the same db
-// This value cannot be changed once installed, unless you want something to break!
-$config['db']['prefix'] = 'cr_';
-
 // ------------------------ RENAME FILE: ------------------------
 //  rename this file to "database.php"
 
